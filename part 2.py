5127317b451cc7f94cb8a4dc5e6e5e55c5e691d5
https://github.com/profshahulsulthan-glitch/civil/commits?author=profshahulsulthan-glitch <profshahulsulthan@gmail.com>
2026-02-24 14:46:29 +0530

    created part 2.py file

---
+++ part 2.py
@@ -0,0 +1,9 @@
+# Taking input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Adding the numbers
+sum = num1 + num2
+
+# Displaying the result
+print("The sum is:", sum)
